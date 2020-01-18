b0b087f8e0ddf9ac6c03aaf35ee1d2bdbdbf0ff9
Sirag Ahmed <ajj.siraj@gmail.com>
2020-01-18 14:06:14 +0200

    added animation and dynamic jumbotron background change

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    var imgID=0;
+    var imageUrls = ["../img/guitar-background-jumbotron.jpg",
+        "../img/guitar-background-jumbotron-2.jpg",
+        "../img/guitar-background-jumbotron-3.jpg",
+        "../img/guitar-background-jumbotron-4.jpg"
+    ];
+    window.setInterval(function(){
+        $('.jumbotron').removeClass('background-animation');
+
+        if(imgID>=imageUrls.length){
+            imgID=0;
+            $('.jumbotron').css("background-image", "url(" + imageUrls[imgID] + ")");
+            
+            imgID++;
+        }
+        else{
+
+            $('.jumbotron').css("background-image", "url(" + imageUrls[imgID] + ")");
+            imgID++;
+        }
+        $('.jumbotron').addClass('background-animation');
+    }, 5000);
+    
+});
